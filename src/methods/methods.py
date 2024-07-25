ee175f391ff5f38011d7da2e30e60e10d565872a
SerpentTRK <for.serpent@gmail.com>
2024-07-25 16:05:31 +0300

    Наполняем тест 001

--- src/methods/methods.py
+++ src/methods/methods.py
@@ -13,21 +13,21 @@ class Methods():
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, locator, time=10):
+    def find_element(self, locator, time=2):
         try:
             return WebDriverWait(self.driver, time).until(lambda driver: driver.find_element(*locator))
         except TimeoutException:
             print(f"Элемент с локатором {locator} не был найден за {time} секунд")
             return None
 
-    def find_elements(self, locator, time=10):
+    def find_elements(self, locator, time=2):
         try:
             return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(*locator))
         except TimeoutException:
             print(f"Элемент с локатором {locator} не был найден за {time} секунд")
             return None
 
-    def  check_browser(self):
+    def check_browser(self):
         """
         Закрываем окно проверки пробузера. Локатор разместил тут, т.к. метод универсальный
         """
@@ -60,3 +60,40 @@ class Methods():
 
             # Пример действия: свайп
             action.drag_and_drop_by_offset(element, 0, 300).perform()
+
+    def swipe_down(self, repeat_n_times=1):
+        """
+        Скроллим страницу вниз
+
+         start_y = берем 80% высоты экрана и используем это значение как начальную точку y для свайпа вниз
+         end_y = 20% высоты экрана и используется как конечная точка y для свайпа
+         start_x = мы берем половину ширины экрана и используем это значение как x координату для свайпа. Таким образом,
+        свайп будет происходить по центру экрана по горизонтали.
+        """
+        size = self.driver.get_window_size()
+
+        start_y = size['height'] * 0.8  # 1929
+        end_y = size['height'] * 0.2  # 482
+        start_x = size['width'] / 2
+
+        for each in range(1, repeat_n_times):
+            self.driver.swipe(start_x, start_y, start_x, end_y, 800)
+
+    def swipe_up(self, repeat_n_times=1):
+        """
+        Скроллим страницу вверх
+
+         start_y = берем 80% высоты экрана и используем это значение как начальную точку y для свайпа вниз
+         end_y = 20% высоты экрана и используется как конечная точка y для свайпа
+         start_x = мы берем половину ширины экрана и используем это значение как x координату для свайпа. Таким образом,
+        свайп будет происходить по центру экрана по горизонтали.
+        """
+        size = self.driver.get_window_size()
+
+        print(size['height'], size['height'] * 0.2, size['height'] * 0.8)
+        start_y = size['height'] * 0.2 # 482
+        end_y = size['height'] * 0.8  # 1929
+        start_x = size['width'] / 2
+
+        for each in range(1, repeat_n_times):
+            self.driver.swipe(start_x, start_y, start_x, end_y, 800)
--- src/test_workspase/create_or_enter_user_account_001.py
+++ src/test_workspase/create_or_enter_user_account_001.py
@@ -14,34 +14,14 @@ class CreateOrEnterUserAccount(Methods):
         self.check_browser()
         self.push_button_remind_me_later()
         self.swipe_banner()
-
         self.user_accaunt()
-        self.swipe_down()
-        self.swipe_up()
+        self.swipe_down(5)
+        self.swipe_up(5)
 
-        time.sleep(5)
+
         self.driver.press_keycode(4)  # -> BACK
-
-
+        time.sleep(0.5)
 
     def user_accaunt(self):
-        my_accaun = self.find_element(UserAccountLocators.LOCATOR_CREATE_OR_ENTER_USER_ACCAUNT)
-        my_accaun.click()
-
-    def swipe_down(self):
-        size = self.driver.get_window_size()
-        # берем 80% высоты экрана и используем это значение как начальную точку y для свайпа вниз
-        start_y = size['height'] * 0.8
-        # 20% высоты экрана и используется как конечная точка y для свайпа
-        end_y = size['height'] * 2
-        # мы берем половину ширины экрана и используем это значение как x координату для свайпа. Таким образом,
-        # свайп будет происходить по центру экрана по горизонтали.
-        start_x = size['width'] / 2
-        self.driver.swipe(start_x, start_y, start_x, end_y, 800)
-
-    def swipe_up(self):
-        size = self.driver.get_window_size()
-        start_y = size['height'] * 2
-        end_y = size['height'] * 0.8
-        start_x = size['width'] / 2
-        self.driver.swipe(start_x, start_y, start_x, end_y, 800)
+        my_accaunt = self.find_element(UserAccountLocators.LOCATOR_CREATE_OR_ENTER_USER_ACCAUNT)
+        my_accaunt.click()
